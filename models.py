44b4fc5124c0b22d8de99e5b089043ed745dda7f
nadia <nadia.elmfeddel@gmail.com>
2023-01-14 02:47:04 +0400

    initial commit

--- models.py
+++ models.py
@@ -18,3 +18,18 @@ class User(db.Model) :
     first_name = db.Column(db.String(50), nullable = False)
     last_name = db.Column(db.String(50))
     image_url = db.Column(db.String, default = default_image)
+    posts = db.relationship('Post')
+    
+
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.Text, nullable = False)
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime,  default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    user = db.relationship('User')
+   
